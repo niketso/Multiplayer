93e8323fe18bd8fab450c5745057111eabb0a578
nicolas <nicolaspiccitto@gmail.com>
2019-09-02 20:00:06 -0300

    MP-v0.1
    init project.

---
+++ UDP Example/Assets/Scripts/UI/ChatScreen.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Net;
+
+public class ChatScreen : MonoBehaviourSingleton<ChatScreen>
+{
+    public Text messages;
+    public InputField inputMessage;
+
+    protected override void Initialize()
+    {
+        inputMessage.onEndEdit.AddListener(OnEndEdit);        
+
+        this.gameObject.SetActive(false);
+
+        NetworkManager.Instance.OnReceiveEvent += OnReceiveDataEvent;
+    }
+
+    void OnReceiveDataEvent(byte[] data, IPEndPoint ep)
+    {
+        if (NetworkManager.Instance.isServer)
+        {
+            NetworkManager.Instance.Broadcast(data);
+        }
+
+        messages.text += System.Text.ASCIIEncoding.UTF8.GetString(data) + System.Environment.NewLine;
+    }
+
+    void OnEndEdit(string str)
+    {
+        if (inputMessage.text != "")
+        {
+            if (NetworkManager.Instance.isServer)
+            {
+                NetworkManager.Instance.Broadcast(System.Text.ASCIIEncoding.UTF8.GetBytes(inputMessage.text));
+                messages.text += inputMessage.text + System.Environment.NewLine;
+            }
+            else
+            {
+                NetworkManager.Instance.SendToServer(System.Text.ASCIIEncoding.UTF8.GetBytes(inputMessage.text));
+            }            
+
+            inputMessage.ActivateInputField();
+            inputMessage.Select();        
+            inputMessage.text = "";
+        }
+
+    }
+
+}
